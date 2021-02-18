2c8fa03650a642983b174e8d28febbb8cb26d298
julian <cassanderburn@gmail.com>
2021-02-18 13:51:02 +0100

    get url sh

---
+++ get_url.sh
@@ -0,0 +1,3 @@
+export NODE_PORT=$(kubectl get --namespace jenkins -o jsonpath="{.spec.ports[0].nodePort}" services jenkins)
+export NODE_IP=$(kubectl get nodes --namespace jenkins -o jsonpath="{.items[0].status.addresses[0].address}")
+echo http://$NODE_IP:$NODE_PORT/login
